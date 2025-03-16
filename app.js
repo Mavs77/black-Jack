5f15785900c9e408479b1f7a9f8c0b38983884db
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2025-03-15 22:43:04 -0500

    card shuffle and initial total

--- app.js
+++ app.js
@@ -5,16 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
 const betAssessment_1 = require("./modules/betAssessment");
+const shuffle_1 = require("./modules/shuffle");
+const cardArray_1 = require("./modules/cardArray");
+const cardDraw_1 = require("./modules/cardDraw");
 const prompt = (0, prompt_sync_1.default)();
 // Player funds starting the game
 let funds = 100;
 console.log('Welcome to Blackjack!');
 // Create spacing
 console.log();
-console.log();
 console.log(`Player's funds: $${funds}`);
 // Ask for the user's name
 let betAmt = parseInt(prompt('Enter your bet: '));
 // Pass the bet amount and player funds to betAssessment
 betAmt = (0, betAssessment_1.betAssessment)(betAmt, funds); // Capture the validated bet
-console.log(`You placed a bet of $${betAmt}.`);
+//Card shuffle and render logic
+let shuffledDeck = (0, shuffle_1.shuffleArray)(cardArray_1.deckOfCards);
+//drawTwo
+let twoCards = (0, cardDraw_1.drawTwoCards)(shuffledDeck);
+let total = twoCards[0].value + twoCards[1].value;
+console.log(`Your hand: ${twoCards[0].name}${twoCards[0].suit},`, `${twoCards[1].name}${twoCards[1].suit}` + ' ', `(Total: ${total})`);
+// console.log(`Your hand: `);
--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import PromptSync from 'prompt-sync';
 import { betAssessment } from './modules/betAssessment';
+import { shuffleArray } from './modules/shuffle';
+import { deckOfCards } from './modules/cardArray';
+import { drawTwoCards } from './modules/cardDraw';
 
 const prompt = PromptSync();
 
@@ -10,7 +13,6 @@ console.log('Welcome to Blackjack!');
 
 // Create spacing
 console.log();
-console.log();
 
 console.log(`Player's funds: $${funds}`);
 
@@ -20,4 +22,18 @@ let betAmt = parseInt(prompt('Enter your bet: '));
 // Pass the bet amount and player funds to betAssessment
 betAmt = betAssessment(betAmt, funds); // Capture the validated bet
 
-console.log(`You placed a bet of $${betAmt}.`);
+//Card shuffle and render logic
+let shuffledDeck = shuffleArray(deckOfCards);
+
+//drawTwo
+let twoCards = drawTwoCards(shuffledDeck);
+
+let total = twoCards[0].value + twoCards[1].value;
+
+console.log(
+  `Your hand: ${twoCards[0].name}${twoCards[0].suit},`,
+  `${twoCards[1].name}${twoCards[1].suit}` + ' ',
+  `(Total: ${total})`
+);
+
+// console.log(`Your hand: `);
